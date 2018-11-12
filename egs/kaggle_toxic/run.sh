4abf6e1b911a6ccc6fdfb3577a150449e3662551
xuheng <xuheng@btows.com>
2018-11-12 17:51:36 +0800

    fix

--- egs/kaggle_toxic/run.sh
+++ egs/kaggle_toxic/run.sh
@@ -1,20 +1,20 @@
-python main.py -c egs/kaggle_toxic/configs/identityhate.train.config
-python main.py -c egs/kaggle_toxic/configs/identityhate.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/identityhate.train.config
+python -d main.py -c egs/kaggle_toxic/configs/identityhate.decode.config
 
-python main.py -c egs/kaggle_toxic/configs/insult.train.config
-python main.py -c egs/kaggle_toxic/configs/insult.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/insult.train.config
+python -d main.py -c egs/kaggle_toxic/configs/insult.decode.config
 
-python main.py -c egs/kaggle_toxic/configs/obscene.train.config
-python main.py -c egs/kaggle_toxic/configs/obscene.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/obscene.train.config
+python -d main.py -c egs/kaggle_toxic/configs/obscene.decode.config
 
-python main.py -c egs/kaggle_toxic/configs/severetoxic.train.config
-python main.py -c egs/kaggle_toxic/configs/severetoxic.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/severetoxic.train.config
+python -d main.py -c egs/kaggle_toxic/configs/severetoxic.decode.config
 
-python main.py -c egs/kaggle_toxic/configs/threat.train.config
-python main.py -c egs/kaggle_toxic/configs/threat.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/threat.train.config
+python -d main.py -c egs/kaggle_toxic/configs/threat.decode.config
 
-python main.py -c egs/kaggle_toxic/configs/toxic.train.config
-python main.py -c egs/kaggle_toxic/configs/toxic.decode.config
+python -d main.py -c egs/kaggle_toxic/configs/toxic.train.config
+python -d main.py -c egs/kaggle_toxic/configs/toxic.decode.config
 
 python egs/kaggle_toxic/submit.py -t data/test.csv \
 --toxic egs/kaggle_toxic/result/toxic_result.csv \
