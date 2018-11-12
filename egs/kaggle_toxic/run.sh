d805ae4fce0d574a2b79616020ebafed08107b4e
xuheng <xuheng@btows.com>
2018-11-12 14:14:58 +0800

    fix

--- egs/kaggle_toxic/run.sh
+++ egs/kaggle_toxic/run.sh
@@ -1,2 +1,17 @@
 python main.py -c egs/kaggle_toxic/configs/identityhate.train.config
 python main.py -c egs/kaggle_toxic/configs/identityhate.decode.config
+
+python main.py -c egs/kaggle_toxic/configs/insult.train.config
+python main.py -c egs/kaggle_toxic/configs/insult.decode.config
+
+python main.py -c egs/kaggle_toxic/configs/obscene.train.config
+python main.py -c egs/kaggle_toxic/configs/obscene.decode.config
+
+python main.py -c egs/kaggle_toxic/configs/severetoxic.train.config
+python main.py -c egs/kaggle_toxic/configs/severetoxic.decode.config
+
+python main.py -c egs/kaggle_toxic/configs/threat.train.config
+python main.py -c egs/kaggle_toxic/configs/threat.decode.config
+
+python main.py -c egs/kaggle_toxic/configs/toxic.train.config
+python main.py -c egs/kaggle_toxic/configs/toxic.decode.config
