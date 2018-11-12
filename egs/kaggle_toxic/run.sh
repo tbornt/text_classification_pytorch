eaa20318848a0891c76715d3102c0e68a197261d
xuheng <xuheng@btows.com>
2018-11-12 14:12:53 +0800

    fix

--- egs/kaggle_toxic/run.sh
+++ egs/kaggle_toxic/run.sh
@@ -1,2 +1,2 @@
-python main.py -c egs/kaggle_toxic/configs/identityhate_train.config
-python main.py -c egs/kaggle_toxic/configs/identityhate_decode.config
+python main.py -c egs/kaggle_toxic/configs/identityhate.train.config
+python main.py -c egs/kaggle_toxic/configs/identityhate.decode.config
